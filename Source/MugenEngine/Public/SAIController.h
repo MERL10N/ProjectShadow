70ae531926c9f26802885222b52eb7623026934a
puppy7777 <whe12345678910@gmail.com>
2025-08-21 10:07:54 -0700

    Added GunBettles to AI Logic
    Now target BlackArms soliders, which can also target them. Same logic as GunHunters. Changed tagging for SAIController from "Enemy" to BlackArmsEnemy

--- Source/MugenEngine/Public/SAIController.h
+++ Source/MugenEngine/Public/SAIController.h
@@ -17,13 +17,14 @@ public:
 	GENERATED_BODY()
 	UPROPERTY(EditDefaultsOnly, Category = "AI")
 	UBehaviorTree* BehaviorTree;
+	void RefreshTarget();
 protected:
 	virtual void BeginPlay() override;
 
 
 	// Updates the target list every second o
 	FTimerHandle TimerHandle_Retarget;
-	void RefreshTarget();
+	
 	
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Target");
 	APawn* MyPawn;
